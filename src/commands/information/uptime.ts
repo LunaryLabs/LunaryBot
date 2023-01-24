36a33bb085300313d56b03cd86590d4ba1a707db
naoexisto bb <machadomarcos2019@gmail.com>
2023-01-24 14:44:13 -0300

    Adicionado uptime.ts

---
+++ src/commands/information/uptime.ts
@@ -0,0 +1,18 @@
+import { SlashCommandBuilder, CommandInteraction } from 'discord.js'
+
+import moment from 'moment'
+
+export class Command {
+  data: SlashCommandBuilder
+  
+  constructor() {
+    this.data = new SlashCommandBuilder()
+      .setName('uptime')
+      .setDescription('my uptime')
+  }
+  
+  async runner(interaction: CommandInteraction): Promise<any> {
+    const uptime = moment.duration(interaction.client.uptime)
+    interaction.reply(`> ${uptime}`)
+  }
+}
