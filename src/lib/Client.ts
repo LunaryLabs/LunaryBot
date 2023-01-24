e1fc81af0567952f0181b29d8634e485771e74e0
GabriewF <85371036+GabriewF@users.noreply.github.com>
2023-01-24 19:31:28 -0300

    Change to DiscordX

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -1,77 +1,44 @@
-import { ChatInputApplicationCommandData, Client, GatewayIntentBits } from 'discord.js'
-import { readdir } from 'node:fs/promises'
+import { ActivityType, GatewayIntentBits, Partials } from 'discord.js'
 import { join } from 'node:path'
-
-import type { ICommand } from '$types/Command'
-import type { IEvent } from '$types/Event'
-import { pino } from '$lib/Logger'
+import { Client } from 'discordx'
+import { importx, dirname } from '@discordx/importer'
 
 export class Lunary {
   private token: string;
-  private intents: Array<GatewayIntentBits>;
   public client: Client;
-  public commands: Array<ICommand>;
 
-  constructor(token?: string, intents?: Array<GatewayIntentBits>) {
+  constructor(token?: string) {
     this.token = String(token ?? process.env['TOKEN']);
-    this.intents = intents ?? [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMembers,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ];
-    
-    this.commands = new Array();
-    
+
     this.client = new Client({
-      intents: this.intents,
+      // To use only guild command
+      // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+
+      // Discord intents
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+      ],
+
+      // Discord partials
+      partials: [
+        Partials.User,
+        Partials.Channel,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.GuildMember,
+        Partials.ThreadMember,
+      ],
+
+      // Other
+      closeTimeout: 10,
+      failIfNotExists: true,
+      silent: true,
     });
 
     this.login();
-    this.loadCommands();
-    this.loadEvents();
-  }
-
-  async loadCommands(): Promise<void> {
-    const categoriesDir = new URL('../commands/', import.meta.url)
-    const categories = await readdir(categoriesDir);
-
-    for await (const category of categories) {
-      const commandsDir = new URL(category, categoriesDir)
-      const commands = await readdir(commandsDir)
-
-      for await (const command of commands) { //pera vou fazer um cmd de base la
-        const commandClassFile = join(categoriesDir.toString(), category, command)
-        const commandFile = (await import(commandClassFile)).default;
-        const commandClass: ICommand = new commandFile()
-        
-        this.commands.push(commandClass)
-        console.log(`[/] comando ${commandClass.data.name} carregado com sucesso`)
-      }
-    }
-  }
-
-  async loadEvents(): Promise<void> {
-    const categoriesDir = new URL('../events/', import.meta.url);
-    const categories = await readdir(categoriesDir);
-
-    for await (const category of categories) {
-      const eventsDir = new URL(category, categoriesDir);
-      const events = (await readdir(eventsDir)).filter(f => f.endsWith('.ts'));
-
-      for await (const event of events) {
-        const eventClassFile = join(eventsDir.toString(), event);
-        const eventFile = (await import(eventClassFile)).default;
-        const eventClass: IEvent = new eventFile();
-
-        pino.info(`[+] Evento ${eventClass.event} carregado com sucesso`);
-        
-        switch (eventClass.once) {
-          case true: this.client.on(eventClass.event, eventClass.runner);
-          case false: this.client.once(eventClass.event, eventClass.runner);
-        }
-      }
-    }
   }
 
   async login() {
--- src/types/Common.ts
+++
@@ -1,3 +0,0 @@
-import type { ClientEvents } from 'discord.js'
-
-export type ArgsOf<K extends keyof ClientEvents> = ClientEvents[K];
