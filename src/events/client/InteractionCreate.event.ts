5917f7c2a9d876c848959cf534959d0cb16085e9
GabriewF <85371036+GabriewF@users.noreply.github.com>
2023-01-25 07:40:40 -0300

    Fixed Events 2

--- src/events/client/InteractionCreate.event.ts
+++
@@ -1,9 +0,0 @@
-import { Discord, On, ArgsOf, Client } from 'discordx'
-
-@Discord()
-export abstract class InteractionCreateEvent {
-  @On({ event: 'interactionCreate' })
-  async Handle([interaction]: ArgsOf<'interactionCreate'>, client: Client): Promise<void> {
-    await client.executeInteraction(interaction);
-  }
-}
