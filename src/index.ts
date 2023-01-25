9f832f66584a7bedc262309defcb983355681d67
naoexisto bb <machadomarcos2019@gmail.com>
2023-01-25 19:48:07 -0300

    Transport to Main

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { Lunary } from '$lib/Client.js'
-import { EnvLoader } from '$lib/EnvLoader.js';
+import { config } from 'dotenv'
 import { join } from 'node:path';
 import { importx, dirname } from '@discordx/importer'
 
-
-EnvLoader();
+config();
 
 await importx(join(dirname(import.meta.url), '..', '**', '*.{event,command}.js'))
 
--- src/lib/EnvLoader.ts
+++
@@ -1,2 +0,0 @@
-import { config } from 'dotenv'
-export const EnvLoader = () => config();
