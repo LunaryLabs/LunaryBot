0787a2d39f0fae48c0cb1ebdc59f5d14972eec83
GabriewF <85371036+GabriewF@users.noreply.github.com>
2023-01-25 08:56:31 -0300

    Added Jokenpo command

---
+++ src/commands/fun/Jokenpo.command.ts
@@ -0,0 +1,56 @@
+import { random } from "$lib/Common";
+import { ApplicationCommandOptionType, CommandInteraction } from "discord.js";
+import { Client, Discord, Slash, SlashChoice, SlashOption } from "discordx";
+
+@Discord()
+export abstract class JokenpoCommand {
+  @Slash({
+    name: 'joquempo',
+    description: 'Pedra, papel, tesoura!',
+    dmPermission: true
+  })
+  async Handler(
+    @SlashChoice({ name: 'Pedra', value: 'rock' })
+    @SlashChoice({ name: 'Papel', value: 'paper' })
+    @SlashChoice({ name: 'Tesoura', value: 'scissors' })
+    @SlashOption({
+      name: 'escolha',
+      description: 'Qual escolha você ira utilizar..',
+      required: true,
+      type: ApplicationCommandOptionType.String
+    })
+    userChoice: string,
+
+    interaction: CommandInteraction,
+  ) {
+    await interaction.deferReply();
+
+    const botChoices = ["rock", "paper", "scissors"];
+    const botChoice = botChoices[Math.floor(Math.random() * botChoices.length)];
+
+    // 0 = Tie
+    // 1 = Player Wins
+    // 2 = Computer Wins
+    let result = 0;
+
+    if (botChoice == userChoice) result = 0;
+
+    // Rock
+    if (botChoice == 'rock' && userChoice == 'scissors') result = 2;
+    if (userChoice == 'rock' && botChoice == 'scissors') result = 1;
+
+    // Scissors
+    if (botChoice == 'scissors' && userChoice == 'paper') result = 2;
+    if (userChoice == 'scissors' && botChoice == 'paper') result = 1;
+
+
+    if (botChoice == 'paper' && userChoice == 'rock') result = 2;
+    if (userChoice == 'paper' && botChoice == 'rock') result = 1;
+
+    switch (result) {
+      case 1: return await interaction.editReply('Você ganhou!');
+      case 2: return await interaction.editReply('o Robô ganhou!');
+      default: return await interaction.editReply('EMPATE');
+    }
+  }
+}
