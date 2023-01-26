3dc92be13601f49883be7d5fd72b36f73878cb22
naoexisto bb <machadomarcos2019@gmail.com>
2023-01-26 12:18:33 -0300

    voltando

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,12 @@ perfData.set('start', start)
 
 config();
 
-await importx(join(dirname(import.meta.url), '..', '**', '*.{event,command}.js')) //teste
+const dirBase = join(dirname(import.meta.url), '**')
 
+// Import Command and Events
+await importx(
+  join(dirBase, '*.event.{ts,js}'),
+  join(dirBase, '*.command.{ts,js}')
+)
 export const lunary = new Lunary();
 export default lunary
